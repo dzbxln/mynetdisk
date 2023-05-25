8d0623abcde1e76fbc4f794f0f387949e757609c
祜易 <1776252636@qq.com>
2023-05-25 11:47:13 +0800

    准备完成

---
+++ src/layout/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <a-button type="primary">{{year}}</a-button>
+</template>
+<script setup lang="ts">
+    import { defineComponent,ref,reactive } from 'vue'
+    // import type { Ref } from 'vue'
+    const state = reactive({ count: 0 })
+    // const year: Ref<string | number> = ref('2020')
+    const year = ref<string | number>('2020')
+
+    function increment() {
+        state.count++
+    }
+</script>
+<style>
+
+</style>
